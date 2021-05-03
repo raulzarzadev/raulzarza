ee16f78d94622675dd094e3d605ada12709087e6
raulzarzadev <46048980+raulzarzadev@users.noreply.github.com>
2021-05-03 02:02:37 +0000

    Deploying to gh-pages from  @ c68dbe92fab7cdceb2d1250cbf6563dbcb7a9112 🚀

--- server/pages/api/jobs.js
+++ server/pages/api/jobs.js
@@ -154,38 +154,10 @@ const Job = external_mongoose_default.a.model('Job', job);
 // CONCATENATED MODULE: ./pages/api/jobs.js
 
 
-/* import connectDB from '../../middleware/mongodb'
-import bcrypt from '../../middleware/bcrypt'
-import User from '../../models/user'
- */
 
 const getJobs = async (req, res) => {
   const jobs = await models_Job.find();
-  console.log('jobs', jobs);
   return res.json(jobs);
-  /*  
-  if (req.method === 'POST') {
-    // Check if name, email or password is provided
-    const { name, email, password } = req.body
-    if (name && email && password) {
-      try {
-        // Hash password to store it in DB
-        var passwordhash = await bcrypt.sign(password)
-        var job = new Job({
-          name
-        })
-        // Create new job
-        var jobcreated = await job.save()
-        return res.status(200).send(jobcreated)
-      } catch (error) {
-        return res.status(500).send(error.message)
-      }
-    } else {
-      res.status(422).send('data_incomplete')
-    }
-  } else {
-    res.status(422).send('req_method_not_supported')
-  } */
 };
 
 /* harmony default export */ var api_jobs = __webpack_exports__["default"] = (mongodb(getJobs));
